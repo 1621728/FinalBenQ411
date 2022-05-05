1c6bc19250d72ffb9d5cb20236412735f91ca449
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-05-05 12:35:16 -0700

    Fixed bug.

--- Assets/BenStuff/Assets/Scripts/Score.cs
+++ Assets/BenStuff/Assets/Scripts/Score.cs
@@ -16,7 +16,7 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        boidNumber = 0;
+        boidNumber = 1;
     }
 
     // Update is called once per frame
