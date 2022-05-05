092559d7647298efddf303b65220eabd4028950d
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-05-05 13:14:45 -0700

    lowered difficulty level.

--- Assets/BenStuff/Assets/Scripts/EnemySpawner.cs
+++ Assets/BenStuff/Assets/Scripts/EnemySpawner.cs
@@ -40,8 +40,6 @@ public class EnemySpawner : MonoBehaviour
                     SpawnObjectAtRandom();
                     SpawnObjectAtRandom();
                     SpawnObjectAtRandom();
-                    SpawnObjectAtRandom();
-                    SpawnObjectAtRandom();
                     //action.Play();
                 }
             }
