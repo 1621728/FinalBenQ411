19c44ed7a371f38140c7bd507bb91b3270a144bc
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-05-12 13:34:51 -0700

    ....

---
+++ Assets/BenStuff/Assets/Scripts/Check if watched tutorial..cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkifwatchedtutorial : MonoBehaviour
+{
+    public bool checktutorial { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //checktutorial = PlayerPrefs
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
