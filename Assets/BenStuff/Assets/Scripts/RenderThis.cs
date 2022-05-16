b474bc5358b096decdf188a583f80f8d1cb9b9c1
DELL-E5470-LAPT\bambu <bam.bugsbunny@gmail.com>
2022-05-15 20:06:15 -0700

    ...

---
+++ Assets/BenStuff/Assets/Scripts/RenderThis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderThis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
